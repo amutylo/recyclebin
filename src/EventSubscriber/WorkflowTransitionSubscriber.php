f3fe071829e6a6c349a7a5e1b66d7b3eab2b8718
andrii mutylo <amutylo@gmail.com>
2018-01-16 11:35:00 +0200

    update fix event id in event subscriber, use generic event names

--- src/EventSubscriber/WorkflowTransitionSubscriber.php
+++ src/EventSubscriber/WorkflowTransitionSubscriber.php
@@ -4,6 +4,11 @@ namespace Drupal\recyclebin\EventSubscriber;
 
 use Drupal\recyclebin\WorkflowHelperInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\state_machine\Event\WorkflowTransitionEvent;
+use Drupal\state_machine\Plugin\Workflow\WorkflowInterface;
+use Drupal\state_machine\Plugin\Workflow\WorkflowState;
+use Drupal\Core\Entity\EntityPublishedInterface;
+
 /**
  * Event subscriber to handle revisions on workflow-enabled entities.
  */
@@ -26,13 +31,52 @@ class WorkflowTransitionSubscriber implements EventSubscriberInterface {
         $this->workflowHelper = $workflowHelper;
     }
 
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    //use generic event name;
+    return [
+      'recycle.recycled.pre_transition' => 'handleAction',
+    ];
+  }
+
     /**
-     * {@inheritdoc}
+     * Handle action callback from workflow.
+     * @param WorkflowTransitionEvent $event
+     * @throws \Exception
      */
-    public static function getSubscribedEvents() {
-//        return [
-//            'state_machine.pre_transition' => 'handleAction',
-//        ];
+    public function handleAction(WorkflowTransitionEvent $event) {
+        $entity = $event->getEntity();
+
+        // Check the new state is the published one.
+        $is_published_state = $this->isPublishedState($event->getToState(), $event->getWorkflow());
+
+        $fields = $this->workflowHelper->getEntityStateField($entity);
+
+        if ($entity instanceof EntityPublishedInterface) {
+            if ($is_published_state) {
+                $entity->setPublished();
+            }
+            else {
+                $entity->setUnpublished();
+            }
+
+        }
     }
-    
+
+    /**
+     * Checks if a state is set as published in a certain workflow.
+     *
+     * @param \Drupal\state_machine\Plugin\Workflow\WorkflowState $state
+     *   The state to check.
+     * @param \Drupal\state_machine\Plugin\Workflow\WorkflowInterface $workflow
+     *   The workflow the state belongs to.
+     *
+     * @return bool
+     *   TRUE if the state is set as published in the workflow, FALSE otherwise.
+     */
+    protected function isPublishedState(WorkflowState $state, WorkflowInterface $workflow) {
+        return $this->workflowHelper->isWorkflowStatePublished($state->getId(), $workflow);
+    }
 }
